4a1a38f479088d21be0c66b3c7f8a132fd269c93
tumamori6 <tumamori6@gmail.com>
2021-06-22 08:54:29 +0900

    いいねテスト

---
+++ tests/Feature/ArticleTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Article;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ArticleTest extends TestCase
+{
+
+    public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    use RefreshDatabase;
+
+    public function testIsLikedByNull()
+    {
+        $article = factory(Article::class)->create();
+
+        $result = $article->isLikedBy(null);
+
+        $this->assertFalse($result);
+    }
+
+    public function testIsLikedByTheUser()
+    {
+        $article = factory(Article::class)->create();
+
+        $user = factory(User::class)->create();
+
+        $article->likes()->attach($user);
+
+        $result = $article->isLikedBy($user);
+
+        $this->assertTrue($result);
+    }
+
+    public function testIsLikedByAnother()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        $another = factory(User::class)->create();
+        $article->likes()->attach($another);
+
+        $result = $article->isLikedBy($user);
+
+        $this->assertFalse($result);
+    }
+}
